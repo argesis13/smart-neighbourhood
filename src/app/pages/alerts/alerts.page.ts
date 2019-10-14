d85a0caabc03133a9684523b98f6ec44369e9f3f
Catalin.Cimpoeru <catalin.cimpoeru@orange.com>
2019-10-14 07:47:16 +0200

    create alerts page

---
+++ src/app/pages/alerts/alerts.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'alerts',
+  templateUrl: './alerts.page.html',
+  styleUrls: ['./alerts.page.scss'],
+})
+export class AlertsPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
