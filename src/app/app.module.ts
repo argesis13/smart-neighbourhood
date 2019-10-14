a6463e5b15b6cf79d605923a7b8891db0efefcd9
Catalin.Cimpoeru <catalin.cimpoeru@orange.com>
2019-10-14 17:35:31 +0200

    fcm provider

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -15,6 +15,24 @@ import { FormsModule } from '@angular/forms';
 import { VideoPlayer } from '@ionic-native/video-player/ngx';
 import { NFC, Ndef } from '@ionic-native/nfc/ngx';
 import { ReactiveFormsModule } from '@angular/forms';
+import {Firebase} from '@ionic-native/firebase';
+import {AngularFireModule} from 'angularfire2';
+import {AngularFirestoreModule} from 'angularfire2/firestore';
+import {FcmService} from './fcm.service';
+
+const firebase = {
+  apiKey: 'AIzaSyCh7ssnPNbIO3l1c9oaYB85GW6xVtIkWEU',
+  authDomain: 'cartierul-iot.firebaseapp.com',
+  databaseURL: 'https://cartierul-iot.firebaseio.com',
+  projectId: 'cartierul-iot',
+  storageBucket: 'cartierul-iot.appspot.com',
+  messagingSenderId: '738763582066',
+  appId: '1:738763582066:web:bd4c88dd0e319c8d4cb0eb',
+  measurementId: 'G-N1G2XJ4XD2'
+}
+
+
+
 @NgModule({
   imports: [
     BrowserModule,
@@ -22,6 +40,8 @@ import { ReactiveFormsModule } from '@angular/forms';
     HttpClientModule,
     FormsModule,
     ReactiveFormsModule,
+    AngularFireModule.initializeApp(firebase),
+    AngularFirestoreModule,
     IonicModule.forRoot(),
     IonicStorageModule.forRoot(),
     ServiceWorkerModule.register('ngsw-worker.js', {
@@ -29,7 +49,7 @@ import { ReactiveFormsModule } from '@angular/forms';
     })
   ],
   declarations: [AppComponent],
-  providers: [InAppBrowser, SplashScreen, StatusBar, VideoPlayer, NFC, Ndef],
+  providers: [InAppBrowser, SplashScreen, StatusBar, VideoPlayer, NFC, Ndef, Firebase, FcmService],
   bootstrap: [AppComponent]
 })
 export class AppModule {}
